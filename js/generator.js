9969cd371c143fc5e1e84465660f4a70eec75f42
abar654 <andrew.barnett.au@gmail.com>
2020-01-29 14:53:09 +0200

    Written js to collect the data and check it. If there are any errors then the corresponding boxes are highlighted red. Users can hold mouse over input to see the error. Now need to add the ability to make the generated session display appear.

--- js/generator.js
+++ js/generator.js
@@ -1,4 +1,6 @@
 checkCheckboxes();
+document.getElementById("generate-button").onclick = generate;
+let currentSessions = [];
 
 function checkCheckboxes() {
   let inputs = document.getElementsByTagName("input");
@@ -7,4 +9,61 @@ function checkCheckboxes() {
       input.checked = true;
     }
   }
+}
+
+function generate() {
+
+  //Get all the inputs
+  let duration = document.getElementById("in-duration").value;
+  let pace = document.getElementById("in-pace").value;
+  let distance = document.getElementById("in-distance").value;
+  let intensity = document.getElementById("in-intensity").value;
+
+  //Get the session types
+  let sessionTypes = [];
+  let inputs = document.getElementsByTagName("input");
+  for(input of inputs){
+    if(input.type == "checkbox") {
+      if(input.checked === true) {
+        sessionTypes.push(input.value);
+      }
+    }
+  }
+
+  //Check that inputs are invalid
+  let validInputs = true;
+
+  //Check duration
+  if(duration == "" || isNaN(duration) || duration < 0) {
+    document.getElementById("in-duration").setAttribute("style", "border-color:var(--color-fail);");
+    validInputs = false;
+  }
+
+  //Check pace
+  if(pace == "" || isNaN(pace) || pace < 0) {
+    document.getElementById("in-pace").setAttribute("style", "border-color:var(--color-fail);");
+    validInputs = false;
+  }
+
+  //Check distance
+  if(distance != "" && distance < 0) {
+    document.getElementById("in-distance").setAttribute("style", "border-color:var(--color-fail);");
+    validInputs = false;
+  }
+
+  //Check intensity
+  if(intensity != "" && (intensity < 1 || intensity > 10 || !Number.isInteger(intensity))) {
+    document.getElementById("in-intensity").setAttribute("style", "border-color:var(--color-fail);");
+    validInputs = false;
+  }
+
+  //Get a session
+  currentSessions = matchSessions(duration, pace, distance, intensity, sessionTypes);
+
+  //Display the session
+
+}
+
+function matchSessions(duration, pace, distance, intensity, sessionTypes) {
+  return [];
 }
