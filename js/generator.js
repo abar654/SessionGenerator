f018eb7589ca34d246b3026742b24cfda67be08f
abar654 <andrew.barnett.au@gmail.com>
2020-01-30 15:59:10 +0200

    Finished writing a function to generate easy session objects. Now need to test it by implementing the displayRandomSession function. Have decided to write all the functions for generating sessions in their own file so that the main js file just needs to call these and gets back a session or list of sessions.

--- js/generator.js
+++ js/generator.js
@@ -1,3 +1,5 @@
+import * as sessionFunctions from "sessions.js";
+
 checkCheckboxes();
 document.getElementById("generate-button").onclick = generate;
 let currentSessions = [];
@@ -45,28 +47,92 @@ function generate() {
     validInputs = false;
   }
 
-  //Check distance
-  if(distance != "" && distance < 0) {
+  //Check distance, can be thought of as max distance
+  if(distance == "") {
+    distance = Number.MAX_SAFE_INTEGER;
+  } else if(distance < 0) {
     document.getElementById("in-distance").setAttribute("style", "border-color:var(--color-fail);");
     validInputs = false;
   }
 
-  //Check intensity
-  if(intensity != "" && (intensity < 1 || intensity > 10 || !Number.isInteger(intensity))) {
+  //Check intensity, if none given then set to -1
+  if(intensity == "") {
+    intensity = -1;
+  } else if(intensity < 1 || intensity > 10 || !Number.isInteger(intensity)) {
     document.getElementById("in-intensity").setAttribute("style", "border-color:var(--color-fail);");
     validInputs = false;
   }
 
-  //Get a session
-  currentSessions = matchSessions(duration, pace, distance, intensity, sessionTypes);
+  if(validInputs) {
 
-  //Display the session
-  document.getElementById("generated-session").setAttribute("style", "display: flex;");
-  document.getElementById("again-button").setAttribute("style", "display: initial;");
-  document.getElementById("instruction-text").setAttribute("style", "display: none;");
+    //Display the session-container
+    document.getElementById("generated-session").setAttribute("style", "display: flex;");
+    document.getElementById("again-button").setAttribute("style", "display: initial;");
+    document.getElementById("again-button").onclick = displayRandomSession();
+    document.getElementById("instruction-text").setAttribute("style", "display: none;");
+
+    //Generate the sessions, then get a session and display it
+    currentSessions = matchSessions(duration, pace, distance, intensity, sessionTypes);
+    displayRandomSession();
+
+  }
 
 }
 
 function matchSessions(duration, pace, distance, intensity, sessionTypes) {
-  return [];
+  let newSessions = [];
+
+  if(sessionTypes.includes("regular")) {
+    newSessions = newSessions.concat(generateRegular(duration, pace, distance, intensity));
+  }
+
+  if(sessionTypes.includes("intervals")) {
+    newSessions = newSessions.concat(generateIntervals(duration, pace, distance, intensity));
+  }
+
+  if(sessionTypes.includes("threshold")) {
+    newSessions = newSessions.concat(generateThreshold(duration, pace, distance, intensity));
+  }
+
+  return newSessions;
+}
+
+function generateRegular(duration, pace, distance, intensity) {
+  let regularSessions = [];
+
+  //Generate easy
+  let easy = sessionFunctions.generateEasySession(duration, pace, distance, intensity);
+  if(easy != null) {
+    regularSessions.push(easy);
+  }
+  
+  //Generate normal
+
+  //Genetate terrain
+
+  //Generate long
+
+  //Generate progression run
+
+  return regularSessions;
+}
+
+function generateIntervals(duration, pace, distance, intensity) {
+  let intervalsSessions = [];
+
+
+  return intervalsSessions;
+}
+
+function generateThreshold(duration, pace, distance, intensity) {
+  let thresholdSessions = [];
+
+
+  return thresholdSessions;
+}
+
+
+//Takes a random session from the currentSessions array and displays one.
+function displayRandomSession() {
+
 }
---
+++ js/sessions.js
@@ -0,0 +1,35 @@
+export function generateEasySession(duration, pace, distance, intensity) {
+
+  let session = {};
+
+  if(intensity == 2) {
+    pace = pace * 1.1;
+    session["intensity"] = 2;
+  } else if(intensity == 3 || intensity == -1) {
+    session["intensity"] = 3;
+  } else if(intensity == 4) {
+    pace = pace * 0.9;
+    session["intensity"] = 4;
+  } else {
+    return null;
+  }
+
+  session["type"] = "Regular";
+  session["name"] = "Easy";
+  session["pace"] = pace;
+
+  let calculatedDist = duration / pace;
+
+  if(calculatedDist < distance) {
+    session["duration"] = duration;
+    session["distance"] = calculatedDist;
+  } else {
+    session["duration"] = distance * pace;
+    session["distance"] = distance;
+  }
+
+  session["comment"] = "Don't push too hard. The aim of this session is to get some training miles without putting too much load on your body. You should be able to talk effortlessly while running and the speed should be one that you could maintain for many hours.";
+
+  return session;
+
+}
